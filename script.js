0439d08679e53505eec6a22c133f8870d236ad4c
AdeelChangezi <106065970+AdeelChangezi@users.noreply.github.com>
2022-06-06 17:41:31 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('.menuButton');
-const nav = document.querySelector('.navItems');
-const navLinks = document.querySelectorAll('.navItems a');
-
-function navSlide() {
-    nav.classList.toggle('navItems-active');
-    menu.classList.toggle('toggle');
-}
